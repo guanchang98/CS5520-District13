1905acb2d10b127f38db9a62366443a2471d2a33
guanchang98 <guanchangst@163.com>
2022-10-21 14:22:57 -0700

    Test with multiple results in response

--- app/src/main/java/com/example/district13/APICall.java
+++ app/src/main/java/com/example/district13/APICall.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 
 public interface APICall {
     // Base URL: https://api.punkapi.com/
-    @GET("v2/beers/192")
+    @GET("v2/beers?brewed_before=11-2012&abv_gt=6")
     Call<JSONResponse[]> getPunks();
 
 }
