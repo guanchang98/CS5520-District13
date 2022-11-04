a409c7f27723e871378e0b47af4cf4b54c0c47f4
guanchang98 <guanchangst@163.com>
2022-11-04 11:35:24 -0700

    Create a login page and navigate to the sticker activity

--- app/src/main/java/com/example/district13/StickerActivity.java
+++ app/src/main/java/com/example/district13/StickerActivity.java
@@ -1,44 +1,23 @@
 package com.example.district13;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.IOException;
 
 public class StickerActivity extends AppCompatActivity {
     private static final String TAG = "StickerActivity";
-
-    // This is the client registration token
-    private static String CLIENT_REGISTRATION_TOKEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker);
-
-        // Generate the token for the first time, then no need to do later
-        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
-            @Override
-            public void onComplete(@NonNull Task<String> task) {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(StickerActivity.this, "Fetching FCM registration token failed", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (CLIENT_REGISTRATION_TOKEN == null) {
-                        CLIENT_REGISTRATION_TOKEN = task.getResult();
-                    }
-                    Log.e("CLIENT_REGISTRATION_TOKEN", CLIENT_REGISTRATION_TOKEN);
-                    Toast.makeText(StickerActivity.this, "CLIENT_REGISTRATION_TOKEN Existed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.getReference("message").child("test").setValue("Hello, World!");
     }
 
 
---
+++ app/src/main/java/com/example/district13/StickerLogInActivity.java
@@ -0,0 +1,30 @@
+package com.example.district13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class StickerLogInActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_log_in);
+    }
+
+    public void showStickerPage(View view) {
+        Intent intent = new Intent(this, StickerActivity.class);
+        editText = findViewById(R.id.editText_login_username);
+        Log.d("StickerLogInActivity", editText.getText().toString());
+        String username = editText.getText().toString();
+        intent.putExtra("Username", username);
+        startActivity(intent);
+    }
+}
