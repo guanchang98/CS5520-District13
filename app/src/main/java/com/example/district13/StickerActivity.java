a4cd94cc18a67c5afcc47973d9cbd7274a117125
guanchang98 <guanchangst@163.com>
2022-11-04 12:01:11 -0700

    Display username on Sticker activity page

--- app/src/main/java/com/example/district13/StickerActivity.java
+++ app/src/main/java/com/example/district13/StickerActivity.java
@@ -2,7 +2,10 @@ package com.example.district13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,10 +14,18 @@ import java.io.IOException;
 public class StickerActivity extends AppCompatActivity {
     private static final String TAG = "StickerActivity";
 
+    TextView welcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("Username");
+        welcomeText = findViewById(R.id.textView_sticker_welcome);
+        welcomeText.setText(getString(R.string.text_sticker_welcome_user, username));
+
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         database.getReference("message").child("test").setValue("Hello, World!");
