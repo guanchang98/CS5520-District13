d8697a34052e8c23ec8787a3fcf85c7d895e9186
guanchang98 <guanchangst@163.com>
2022-11-07 15:44:00 -0800

    Display user list correctly and start creating notification for any changes in received_stickers list

--- app/src/main/java/com/example/district13/StickerActivity.java
+++ app/src/main/java/com/example/district13/StickerActivity.java
@@ -1,6 +1,8 @@
 package com.example.district13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,7 +25,12 @@ import com.example.district13.sticker_image.StickerImageAdapter;
 import com.example.district13.sticker_user.StickerUser;
 import com.example.district13.sticker_user.StickerUserAdapter;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,6 +40,8 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class StickerActivity extends AppCompatActivity {
@@ -46,21 +55,60 @@ public class StickerActivity extends AppCompatActivity {
     List<String> selectedUsers;
     List<String> selectedImagePaths;
 
+    String username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker);
 
         Intent intent = getIntent();
-        String username = intent.getStringExtra("Username");
+        username = intent.getStringExtra("Username");
         welcomeText = findViewById(R.id.textView_sticker_welcome);
         welcomeText.setText(getString(R.string.text_sticker_welcome_user, username));
 
         userList = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            String cur = "user" + i;
-            userList.add(new StickerUser(cur));
-        }
+//        for (int i = 0; i < 30; i++) {
+//            String cur = "user" + i;
+//            userList.add(new StickerUser(cur));
+//        }
+
+        // Write a message to the database
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+//        dbRef.child("users").child("user0").child("sent_stickers").setValue(new ArrayList<>(Arrays.asList("sent_image_0")));
+//        dbRef.child("users").child("user0").child("received_stickers").setValue(new ArrayList<>(Arrays.asList("received_image_0")));
+        dbRef.child("imageURLs").child("0").setValue("0.png");
+        dbRef.child("users").child("user0").child("received_stickers").push().setValue("sticker3");
+        dbRef.child("users").orderByKey().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Log.d(TAG, data.getKey());
+                    StickerUser stickerUser = new StickerUser(data.getKey());
+                    userList.add(stickerUser);
+                }
+                usersRecyclerView = findViewById(R.id.recyclerView_sticker_user);
+
+                usersRecyclerView.setHasFixedSize(true);
+                //This defines the way in which the RecyclerView is oriented
+                usersRecyclerView.setLayoutManager(new LinearLayoutManager(StickerActivity.this));
+
+                //Associates the adapter with the RecyclerView
+                usersRecyclerView.setAdapter(new StickerUserAdapter(userList, StickerActivity.this));
+
+                Log.v(TAG, userList.size() + " before");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                // ...
+            }
+        });
+
+
+
 
         Log.v(TAG, "Initialize sticker list...");
         stickerList = new ArrayList<>();
@@ -77,17 +125,44 @@ public class StickerActivity extends AppCompatActivity {
         selectedUsers = new ArrayList<>();
         selectedImagePaths = new ArrayList<>();
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        usersRecyclerView = findViewById(R.id.recyclerView_sticker_user);
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+                String cur = (String) dataSnapshot.getValue();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(StickerActivity.this, "channel_id")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("Receive new sticker")
+                        .setContentText("Someone send you a new sticker " + cur)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        usersRecyclerView.setHasFixedSize(true);
-        //This defines the way in which the RecyclerView is oriented
-        usersRecyclerView.setLayoutManager(new LinearLayoutManager(StickerActivity.this));
+            }
 
-        //Associates the adapter with the RecyclerView
-        usersRecyclerView.setAdapter(new StickerUserAdapter(userList, StickerActivity.this));
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+            }
+        };
+        dbRef.child("users").child(username).child("received_stickers").addChildEventListener(childEventListener);
+
+
     }
 
     public void showSelectStickersDialog(View view) {
@@ -107,9 +182,7 @@ public class StickerActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // TODO: Add all selected stickers' paths to all selected users
-//                Snackbar snackbar;
-//                snackbar = Snackbar.make(findViewById(R.id.snackbar_sticker_dialog), R.string.textView_sticker_dialog_send_success, 2000);
-//                snackbar.show();
+                Log.v(TAG, userList.size() + " after");
                 Toast.makeText(StickerActivity.this, "Send stickers successfully！", Toast.LENGTH_SHORT).show();
             }
         });
